93877234a194b29b7a2be45aad4787ecb56f4fdb
GreatVV <marinegreatvv@yahoo.com>
2013-09-17 01:56:06 +0700

    Add drag

--- Assets/Scripts/Level/gamefield.cs
+++ Assets/Scripts/Level/gamefield.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
-public class gamefield : MonoBehaviour {
+public class Gamefield : MonoBehaviour {
 
     public GameObject ChuzzlePrefab;
     public Vector3 ChuzzleScale = new Vector3(1,1,1);
@@ -9,21 +12,138 @@ public class gamefield : MonoBehaviour {
     public int Width = 7;
     public int Height = 10;
 
+    public List<Chuzzle> chuzzles;
+
+    public LayerMask chuzzleMask;
+
     void Awake()
     {
         for (int i = 0; i < Height; i++)
-        {
-            var row = new GameObject("Row"+(i+1));
-            row.transform.parent = this.gameObject.transform;
-            row.transform.localPosition = new Vector3(0, i * ChuzzleScale.y, 0);
-            row.transform.localRotation = Quaternion.Euler(new Vector3(90, 0, 0));
+        {           
             for (int j = 0; j < Width; j++)
             {
-                var gameObject = NGUITools.AddChild(row, ChuzzlePrefab);
+                var gameObject = NGUITools.AddChild(this.gameObject, ChuzzlePrefab);
+                gameObject.layer = ChuzzlePrefab.layer;
+                var chuzzle = gameObject.GetComponent<Chuzzle>();
+                chuzzle.x = j;
+                chuzzle.y = i;
                 gameObject.transform.localScale = ChuzzleScale;
-                gameObject.transform.localPosition = new Vector3(j * ChuzzleScale.x, 0, 0);
+                gameObject.transform.localPosition = new Vector3(j * ChuzzleScale.x, i*ChuzzleScale.y, 0);
+                gameObject.transform.localRotation = Quaternion.Euler(new Vector3(90, 0, 0));
+
+                chuzzles.Add(chuzzle);
             }
         }
     }
 
+    bool isDrag;    
+    Chuzzle draggable;
+    Vector3 dragOrigin;
+    Vector3 delta;
+    Vector3 deltaTouch;
+    bool directionChozen;
+    bool isVerticalDrag;
+
+    public Chuzzle currentChuzzle;
+    public List<Chuzzle> selectedChuzzles;
+
+    void Update()
+    {
+        isDrag = false;
+
+        // MOUSE WAS DOWN
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+
+            Ray ray = Camera.main.ScreenPointToRay(dragOrigin);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, float.MaxValue, chuzzleMask))
+            {
+                currentChuzzle = hit.transform.gameObject.GetComponent<Chuzzle>();
+            }
+
+            return;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            DropDrag();
+            return;
+        }
+
+        if (currentChuzzle == null)
+        {
+            return;
+        }
+
+        // TOUCH WAS DOWN
+        else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            dragOrigin = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 0);
+            return;
+        }
+
+        // CHECK DRAG STATE (Mouse or Touch)
+        if (!Input.GetMouseButton(0))
+            if (0 == Input.touchCount || Input.GetTouch(0).phase != TouchPhase.Moved)
+            {
+                DropDrag();
+                return;
+            }
+  
+        // Get Viewport Position Difference between Last and Current Touches
+        if (Input.GetMouseButton(0))
+        {	// MOUSE
+            delta = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            { // TOUCH
+                deltaTouch = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 0);
+                delta = Camera.main.ScreenToViewportPoint(deltaTouch - dragOrigin);
+            }
+        }
+
+        if (!directionChozen)
+        {
+            if (Mathf.Abs(delta.x) < 3 * Mathf.Abs(delta.y) || Mathf.Abs(delta.x) > 3 * Mathf.Abs(delta.y))
+            {
+                if (Mathf.Abs(delta.x) < Mathf.Abs(delta.y))
+                {
+                    //TODO: choose row
+                    selectedChuzzles = chuzzles.Where(x => x.x == currentChuzzle.x).ToList();
+                    isVerticalDrag = true;
+                }
+                else
+                {
+                    //TODO: choose column
+                    selectedChuzzles = chuzzles.Where(x => x.y == currentChuzzle.y).ToList();
+                    isVerticalDrag = false;
+                }
+
+                directionChozen = true;
+            }           
+        }
+        else
+        {
+            foreach (var c in selectedChuzzles)
+            {
+                c.transform.localPosition += isVerticalDrag ? new Vector3(0, delta.y, 0) * 10 : new Vector3(delta.x, 0, 0) * 10;
+            }
+        }
+
+        // RESET START POINT
+        dragOrigin = Input.mousePosition;
+
+        isDrag = true;
+    }
+
+    private void DropDrag()
+    {
+        selectedChuzzles.Clear();
+        currentChuzzle = null;
+        directionChozen = false;
+    }
 }
